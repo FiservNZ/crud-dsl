f0e0ac2a021162ae149ac9486961af5c88c048d9
Gareth Shaw <gareth@garethnz.com>
2019-09-14 23:30:19 +1200

    Use moshi, do some GET/POST/PUSH/DELETEs

--- src/crud-dsl-main.kt
+++ src/crud-dsl-main.kt
@@ -4,19 +4,27 @@ import okhttp3.OkHttpClient
 fun main(args : Array<String>) {
     fun result() =
         users {
+            exhaustive = true
             user {
-                id = 7
-                email = "michael.lawson@reqres.in"
-                first_name = "Michael"
-                last_name = "Lawson"
-                avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+                id = 1 // Things like primary keys will change. if it doesn't exist, the api to create a new one will not match this id
+                email = "george.bluth@reqres.in"
+                first_name = "George"
+                last_name = "Bluth"
+                avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
             }
             user {
-                id = 8
-                email = "lindsay.ferguson@reqres.in"
-                first_name = "Lindsay"
-                last_name = "Ferguson"
-                avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
+                id = 6
+                email = "tracey.ramos@newemail.com"
+                first_name = "Tracey"
+                last_name = "Ramos"
+                avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
+            }
+            user {
+                id = 200
+                email = "gareth@garethnz.com"
+                first_name = "Gareth"
+                last_name = "NZ"
+                avatar = "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
             }
         }
 //    println("Hello, World!")
--- src/reqres-dsl.kt
+++ src/reqres-dsl.kt
@@ -1,19 +1,20 @@
 package com.example.reqres
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.Response
+import okhttp3.RequestBody
+
+val MEDIA_TYPE_JSON = "application/json; charset=utf-8".toMediaType()
 
 // https://reqres.in/
 interface Element {
     fun render(builder: StringBuilder, indent: String)
     fun applyToServer(client: OkHttpClient)
-}
-
-abstract class TextElement(val text: String) : Element {
-    override fun render(builder: StringBuilder, indent: String) {
-        builder.append("$indent$text\n")
-    }
 }
 
 @DslMarker
@@ -31,17 +32,20 @@ abstract class Tag(val name: String) : Element {
     }
 
     override fun render(builder: StringBuilder, indent: String) {
-        builder.append("$indent<$name${renderAttributes()}>\n")
+        builder.append("$indent$name {${renderAttributes(indent+"  ")}")
         for (c in children) {
             c.render(builder, indent + "  ")
         }
-        builder.append("$indent</$name>\n")
+        builder.append("$indent}\n")
     }
 
-    private fun renderAttributes(): String {
-        val builder = StringBuilder()
+    private fun renderAttributes(indent: String): String {
+        val builder = StringBuilder("\n")
+        if (attributes.size == 0) {
+            return builder.toString()
+        }
         for ((attr, value) in attributes) {
-            builder.append(" $attr=\"$value\"")
+            builder.append("$indent$attr = \"$value\"\n")
         }
         return builder.toString()
     }
@@ -53,28 +57,93 @@ abstract class Tag(val name: String) : Element {
     }
 }
 
+data class UsersResponse( val page: Int,
+                         val per_page: Int,
+                         val total: Int,
+                         val total_pages: Int,
+                         val data: Array<User>
+                         ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UsersResponse
+
+        if (page != other.page) return false
+        if (per_page != other.per_page) return false
+        if (total != other.total) return false
+        if (total_pages != other.total_pages) return false
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = page
+        result = 31 * result + per_page
+        result = 31 * result + total
+        result = 31 * result + total_pages
+        result = 31 * result + data.contentHashCode()
+        return result
+    }
+}
+
 class UserList : Tag("userlist") {
     fun user(init: User.() -> Unit) = initTag(User(), init)
     val url = "https://reqres.in/api/users"
+    var exhaustive: Boolean
+        get() = attributes["exhaustive"]!!.toBoolean()
+        set(value) {
+            attributes["exhaustive"] = value.toString()
+        }
     override fun applyToServer(client: OkHttpClient) {
         // This would compare children to the list returned from https://reqres.in/api/users
         var request = Request.Builder()
             .url(url)
             .build();
 
+        val moshi = Moshi.Builder()
+            // ... add your own JsonAdapters and factories ...
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        val jsonAdapter = moshi.adapter<UsersResponse>(UsersResponse::class.java!!)
+
+        var usersResponse: UsersResponse?
         client.newCall(request).execute().apply {
             println(this.request.url.toUrl().toString())
-            println(this.body?.string())
+            usersResponse = jsonAdapter.fromJson(this.body?.source())
+            //println( usersResponse )
+        }
+        val childUsersToProcess = children.filterIsInstance<User>().toMutableList()
+
+        usersResponse?.let {
+            it.data.forEach { user ->
+                    // If it matches a child we want, then sync to it
+                    val matchingChildUser = childUsersToProcess.firstOrNull { childUser -> childUser.primaryKeyEquals(user) }
+                    if (matchingChildUser != null) {
+                        matchingChildUser.applyToServer(client, user)
+                        childUsersToProcess.remove(matchingChildUser)
+                    }
+                    else {
+
+                        if (exhaustive) {
+                            // TODO: Delete user from the server as it doesn't match something we want to add
+                            user.delete(client)
+                        }
+                    }
+            }
         }
 
-        children.forEach { element: Element -> element.applyToServer(client) }
+        childUsersToProcess.forEach { element: Element -> element.applyToServer(client) }
         // CREATE CHILD, LET CHILD Update the existing instance, DELETE user on server
     }
 }
 
 class User : Tag("user") {
-    var id: Int
-        get() = attributes["id"]!!.toInt()
+    val idmatch: Boolean
+        get() = attributes.containsKey("id")
+    var id: Int?
+        get() = attributes["id"]?.toInt()
         set(value) {
             attributes["id"] = value.toString()
         }
@@ -99,18 +168,105 @@ class User : Tag("user") {
             attributes["avatar"] = value
         }
 
-    val url = "https://reqres.in/api/users/"
     override fun applyToServer(client: OkHttpClient) {
+        // TODO: If this isn't running as part of a LIST, targetUser needs to be GET'd
+        applyToServer(client, null)
+    }
+
+
+    val url = "https://reqres.in/api/users/"
+    /**
+     * user is a user from the server's response that 'approximatelyEquals' returns true
+     */
+    fun applyToServer(client: OkHttpClient, targetUser: User?) {
+        var createTputF = true
         // This would compare an instance of user from https://reqres.in/api/users/2 with the current object. CREATE / UPDATE as needed
-        var request = Request.Builder()
-            .url(url+id)
-            .build();
+        // Just take the ID of this and then do a PUT? if there are any other differences
+        targetUser?.let {
+            id = it.id
+            createTputF = false
+        }
+
+        if (this.equals(targetUser)) {
+            println("User $email already exists and is equal. Skipping")
+            return // No Actions Needed
+        }
+
+        val moshi = Moshi.Builder()
+            // ... add your own JsonAdapters and factories ...
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        val jsonAdapter = moshi.adapter<User>(User::class.java!!)
+
+        val jsonUser = jsonAdapter.toJson(this)
+
+
+
+        var request : Request;
+        if (createTputF) {
+            request = Request.Builder()
+                .url(url+id) // TODO does this type of API usually work? i.e. requesting the ID of a new object?
+                .post(jsonUser.toRequestBody(MEDIA_TYPE_JSON))
+                .build();
+        } else {
+            request = Request.Builder()
+                .url(url+id)
+                .put(jsonUser.toRequestBody(MEDIA_TYPE_JSON))
+                .build();
+        }
 
         client.newCall(request).execute().apply {
             println(this.request.url.toUrl().toString())
             println(this.body?.string())
         }
     }
+
+    fun delete(client: OkHttpClient) {
+        val moshi = Moshi.Builder()
+            // ... add your own JsonAdapters and factories ...
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        val jsonAdapter = moshi.adapter<User>(User::class.java!!)
+
+        val jsonUser = jsonAdapter.toJson(this)
+
+        var request = Request.Builder()
+                .url(url+id) // TODO does this type of API usually work? i.e. requesting the ID of a new object?
+                .delete(jsonUser.toRequestBody(MEDIA_TYPE_JSON))
+                .build();
+
+        client.newCall(request).execute().apply {
+            println("User $id deleted successfully")
+            println(this.request.url.toUrl().toString())
+            println(this.body?.string())
+        }
+    }
+
+    fun primaryKeyEquals(other: User): Boolean {
+
+        if (id == null)
+            return false
+        if (id != other.id)
+            return false
+        return true
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        // TODO: if (attributes.keys.map)
+        if (id != other.id) return false
+        if (first_name != other.first_name) return false
+        if (last_name != other.last_name) return false
+        if (email != other.email) return false
+        if (avatar != other.avatar) return false
+        return true
+    }
+
+
 }
 
 fun users(init: UserList.() -> Unit): UserList {
